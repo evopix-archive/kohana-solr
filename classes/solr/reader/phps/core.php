b95fb606434d99655d8ca4387ad0f73979c3e921
Brandon Summers <brandon@evolutionpixels.com>
2011-03-21 14:07:57 -0700

    Finishing PHPS reader.

--- classes/solr/reader/phps/core.php
+++ classes/solr/reader/phps/core.php
@@ -7,4 +7,42 @@
  * @copyright  (c) 2011 Brandon Summers
  * @license    MIT
  */
-class Solr_Reader_PHPS_Core extends Solr_Reader {}
+class Solr_Reader_PHPS_Core extends Solr_Reader {
+
+	/**
+	 * Parses the serialized php response into an object.
+	 *
+	 * @return  object
+	 */
+	public function parse()
+	{
+		$body = $this->_response->body();
+		return $this->_to_object(unserialize($body));
+	}
+
+	/**
+	 * Converts the multidimensional response array into an object.
+	 *
+	 * @param   array   $array  multidimensional response array
+	 * @return  object
+	 */
+	protected function _to_object($array)
+	{
+		if (is_array($array) AND (count($array) > 0))
+		{
+			$object = (object) $array;
+
+			foreach ($object as $property => $value)
+			{
+				if (is_array($value) AND (count($value) > 0))
+				{
+					$object->$property = $this->_to_object($value);
+				}
+			}
+
+			return $object;
+		}
+
+		return FALSE;
+	}
+}
