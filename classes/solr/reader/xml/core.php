bd7efaed591cc43473ab232a994fa9c0a4e29037
Brandon Summers <brandon@evolutionpixels.com>
2011-03-21 14:07:44 -0700

    Finishing xml reader.

--- classes/solr/reader/xml/core.php
+++ classes/solr/reader/xml/core.php
@@ -7,4 +7,17 @@
  * @copyright  (c) 2011 Brandon Summers
  * @license    MIT
  */
-class Solr_Reader_XML_Core extends Solr_Reader {}
+class Solr_Reader_XML_Core extends Solr_Reader {
+
+	/**
+	 * Parses the XML response into an object.
+	 *
+	 * @return  object
+	 */
+	public function parse()
+	{
+		$body = $this->_response->body();
+		return simplexml_load_string($body);
+	}
+
+}
