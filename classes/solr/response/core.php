9792a02063ba8ab1bb4f48a73799a5c12d967325
Brandon Summers <brandon@evolutionpixels.com>
2011-03-21 13:25:40 -0700

    Finishing up base response class.

--- classes/solr/response/core.php
+++ classes/solr/response/core.php
@@ -7,4 +7,67 @@
  * @copyright  (c) 2011 Brandon Summers
  * @license    MIT
  */
-class Solr_Response_Core {}
+class Solr_Response_Core {
+
+	/**
+	 * @var  Solr_Reader  reader instance for format
+	 */
+	protected $_reader;
+
+	/**
+	 * @var  array  array of parsed response data
+	 */
+	protected $_data;
+
+	/**
+	 * Overload construct to instantiate the reader.
+	 *
+	 * @return  void
+	 */
+	public function __construct(Response $response)
+	{
+		$this->_reader = Solr_Reader::factory(Solr::$write_response_format, $response);
+		$this->_data = $this->_reader->parse();
+	}
+
+	/**
+	 * Gets the raw parsed response data.
+	 *
+	 * @return  SimpleXMLElement
+	 */
+	public function data()
+	{
+		return $this->_data;
+	}
+
+	/**
+	 * Gets the response status.
+	 *
+	 * @return  integer
+	 */
+	public function status()
+	{
+		return (int) $this->header()->status;
+	}
+
+	/**
+	 * Gets the query time of the response.
+	 *
+	 * @return  integer
+	 */
+	public function query_time()
+	{
+		return (int) $this->header()->QTime;
+	}
+
+	/**
+	 * Gets the full response header.
+	 *
+	 * @return  SimpleXMLElement
+	 */
+	public function header()
+	{
+		return $this->_data->responseHeader;
+	}
+
+}
