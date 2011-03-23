9c0bbe21c1d14c5d399ab3cad95f5b1904757e64
Brandon Summers <brandon@evolutionpixels.com>
2011-03-23 11:58:42 -0700

    Adding remaining common param methods to read request.

--- classes/solr/request/read/core.php
+++ classes/solr/request/read/core.php
@@ -57,6 +57,21 @@ var_export($response->body());
 	}
 
 	/**
+	 * Sets and gets the fq param for the request.
+	 *
+	 * @param   string  $query  the filter query string
+	 * @return  mixed
+	 */
+	public function filter_query($query = NULL)
+	{
+		if ( ! $query)
+			return Arr::get($this->_get, 'fq');
+
+		$this->_get['fq'] = $query;
+		return $this;
+	}
+
+	/**
 	 * Sets and gets the offset param for the request.
 	 *
 	 * @param   integer  $offset  starting offset for documents
@@ -87,6 +102,21 @@ var_export($response->body());
 	}
 
 	/**
+	 * Sets and gets the sort param for the request.
+	 *
+	 * @param   string  $sort  field to sort the documents by
+	 * @return  mixed
+	 */
+	public function sort($sort = NULL)
+	{
+		if ( ! $sort)
+			return Arr::get($this->_get, 'sort');
+
+		$this->_get['sort'] = $sort;
+		return $this;
+	}
+
+	/**
 	 * Sets and gets the fields param for the request.
 	 *
 	 * @param   mixed  $fields  list of fields for the request to return
@@ -103,6 +133,104 @@ var_export($response->body());
 		}
 
 		$this->_get['fl'] = $fields;
+		return $this;
+	}
+
+	/**
+	 * Sets and gets the defType param for the request.
+	 *
+	 * @param   string  $type  the queryParser name
+	 * @return  mixed
+	 */
+	public function type($type = NULL)
+	{
+		if ( ! $type)
+			return Arr::get($this->_get, 'defType');
+
+		$this->_get['defType'] = $type;
+		return $this;
+	}
+
+	/**
+	 * Sets and gets the timeAllowed param for the request.
+	 *
+	 * @param   integer  $time  the time, in milliseconds, for the search to finish
+	 * @return  mixed
+	 */
+	public function time_allowed($time = NULL)
+	{
+		if ( ! $time)
+			return Arr::get($this->_get, 'timeAllowed');
+
+		$this->_get['timeAllowed'] = $time;
+		return $this;
+	}
+
+	/**
+	 * Sets and gets the omitHeader param for the request.
+	 *
+	 * @param   boolean  $value  if TRUE the result header is excluded
+	 * @return  mixed
+	 */
+	public function omit_header($value = NULL)
+	{
+		if ( ! $value)
+			return Arr::get($this->_get, 'omitHeader');
+
+		if ($value === TRUE)
+		{
+			$this->_get['omitHeader'] = 'true';
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Sets and gets the debug param for the request.
+	 *
+	 * @param   boolean  $debug  information to debug
+	 * @return  mixed
+	 */
+	public function debug($debug = NULL)
+	{
+		if ( ! $debug)
+			return Arr::get($this->_get, 'debug');
+
+		$this->_get['debug'] = $debug;
+		return $this;
+	}
+
+	/**
+	 * Sets and gets the debugQuery param for the request.
+	 *
+	 * @param   boolean  $debug  if TRUE, the query will be debugged
+	 * @return  mixed
+	 */
+	public function debug_query($debug = NULL)
+	{
+		if ( ! $debug)
+			return Arr::get($this->_get, 'debugQuery');
+
+		if ($debug === TRUE)
+		{
+			$this->_get['debugQuery'] = 'true';
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Sets and gets the explainOther param for the request.
+	 *
+	 * @param   string  $query  the Lucene query
+	 * @return  mixed
+	 */
+	public function explain_other($query = NULL)
+	{
+		if ( ! $query)
+			return Arr::get($this->_get, 'explainOther');
+
+		$this->_get['explainOther'] = $query;
 		return $this;
 	}
 
