b609fb7fd5c4731ad07495eebb3578736f0191df
Brandon Summers <brandon@evolutionpixels.com>
2011-03-24 17:09:03 -0700

    Updating config to work with mulitple instances.

--- config/solr.php
+++ config/solr.php
@@ -1,10 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-
-	'host' => 'localhost:8983/solr/',
-	'write_format' => 'xml',
-	'write_response_format' => 'xml',
-	'read_format' => 'xml',
-	'read_response_format' => 'xml',
+	'default' => array(
+		'host' => 'localhost:8983/solr/',
+		'writer' => 'xml',
+		'reader' => 'json',
+	),
 );
